422e5403e89077a41f70daf48889403235d23257
Ahmed1ad <96233908+Ahmed1ad@users.noreply.github.com>
2025-12-28 18:21:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const BOT_TOKEN = process.env.BOT_TOKEN;
 const BSCSCAN_API = process.env.BSCSCAN_API;
 
 const WATCHED_ADDRESS =
-  "0xAE02717e94C9B5bAe817601a49B4584F58324015".toLowerCase();
+  "0x238a358808379702088667322f80aC48bAd5e6c4".toLowerCase();
 
 const CHECK_INTERVAL = 15000; // 15 seconds
 const DB_FILE = "./data.json";
